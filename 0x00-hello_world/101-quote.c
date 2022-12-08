41045f756e898849e6ede3c57f14195f9c8d06fe
Shadrack Kiplangat <Sherkiplah@gmail.com>
2022-12-08 12:45:39 -0800

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#!/bin/bash
-#include <stdio.h>
+#!/bin/bash#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -8,7 +7,8 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+        write(STDOUT_FILENO, "and that piece of art is useful
+                        \" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
+
