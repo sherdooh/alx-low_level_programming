1fd8fdfaf5a638168c563b5661bcfbc9bceece25
Shadrack Kiplangat <Sherkiplah@gmail.com>
2022-12-09 14:17:37 -0800

    smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,26 +10,11 @@
 
 int main(void)
 {
-	int d, p;
+	char low;
 
-	for (d = '0'; d < '9'; d++)
-	{
-		for (p = d + 1; p <= '9'; p++)
-		{
-			if (p != d)
-			{
-				putchar(d);
-				putchar(p);
-
-				if (d == '8' && p == '9')
-					continue;
-
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+		putchar('\n');
 
 	return (0);
 }
